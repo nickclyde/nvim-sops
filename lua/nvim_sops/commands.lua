2e7af59ff8a579f363f41348700522108db8e077
Nick Clyde <nick@clyde.tech>
2025-01-23 12:53:11 -0800

    Add more debugging for decryption

--- lua/nvim_sops/commands.lua
+++ lua/nvim_sops/commands.lua
@@ -1,91 +1,102 @@
-local debug = require('nvim_sops.utils').debug
-local sops = require('nvim_sops.sops')
-
+local debug = require("nvim_sops.utils").debug
+local sops = require("nvim_sops.sops")
 
 local M = {}
 
-
 M.file_encrypt = function()
-  -- load lyaml
-  local input_file = vim.fn.expand('%:p')
-  local dir = vim.fn.expand('%:p:h')
-  local project_path = vim.fn.fnamemodify(vim.fn.expand('%'), ':.')
-  debug('decrypting', input_file)
+	-- load lyaml
+	local input_file = vim.fn.expand("%:p")
+	local dir = vim.fn.expand("%:p:h")
+	local project_path = vim.fn.fnamemodify(vim.fn.expand("%"), ":.")
+	debug("decrypting", input_file)
 
-  -- local sops_file = sops.find_sops_config_file(dir)
-  -- local sops_config = parse_yaml_with_yq(sops_file)
-  --
-  -- debug('sops_config', sops_config)
-  --
-  -- if not sops_config or not sops_config.creation_rules then
-  --   print('Invalid .sops.yaml file format, no creation_rules found')
-  --   return nil
-  -- end
-  -- -- find the first creation_rule that matches the current file path
-  -- for _, rule in ipairs(sops_config.creation_rules) do
-  --   local escaped_pattern = escape_lua_pattern(rule.path_regex)
-  --   local match = string.match(project_path, escaped_pattern)
-  --
-  --   if match then
-  --     -- return rule.age
-  --     print('matched rule: ' .. rule.path_regex .. ' with age key: ' .. rule.age)
-  --     print('using escaped pattern: ' .. escaped_pattern)
-  --   else
-  --     print("Error matching path: " .. project_path .. " with regex: " .. rule.path_regex)
-  --     print('using escaped pattern: ' .. escaped_pattern)
-  --   end
-  -- end
-  --
-  local binary = vim.g.nvim_sops_bin_path .. ' '
-  local sops_options = sops.get_sops_general_options()
+	-- local sops_file = sops.find_sops_config_file(dir)
+	-- local sops_config = parse_yaml_with_yq(sops_file)
+	--
+	-- debug('sops_config', sops_config)
+	--
+	-- if not sops_config or not sops_config.creation_rules then
+	--   print('Invalid .sops.yaml file format, no creation_rules found')
+	--   return nil
+	-- end
+	-- -- find the first creation_rule that matches the current file path
+	-- for _, rule in ipairs(sops_config.creation_rules) do
+	--   local escaped_pattern = escape_lua_pattern(rule.path_regex)
+	--   local match = string.match(project_path, escaped_pattern)
+	--
+	--   if match then
+	--     -- return rule.age
+	--     print('matched rule: ' .. rule.path_regex .. ' with age key: ' .. rule.age)
+	--     print('using escaped pattern: ' .. escaped_pattern)
+	--   else
+	--     print("Error matching path: " .. project_path .. " with regex: " .. rule.path_regex)
+	--     print('using escaped pattern: ' .. escaped_pattern)
+	--   end
+	-- end
+	--
+	local binary = vim.g.nvim_sops_bin_path .. " "
+	local sops_options = sops.get_sops_general_options()
 
-  local envs = ""
-  for key, value in pairs(sops_options.sopsGeneralEnvVars) do
-    envs = envs .. key .. '=' .. value .. ' '
-  end
-  local args = {
-    '--encrypt',
-    '--in-place',
-  }
+	local envs = ""
+	for key, value in pairs(sops_options.sopsGeneralEnvVars) do
+		envs = envs .. key .. "=" .. value .. " "
+	end
+	local args = {
+		"--encrypt",
+		"--in-place",
+	}
 
-  local command = envs .. binary .. table.concat(args, ' ') .. ' ' .. input_file
-  debug(command)
+	local command = envs .. binary .. table.concat(args, " ") .. " " .. input_file
+	debug(command)
 
-  vim.fn.system(command)
-  if vim.v.shell_error ~= 0 then
-    print('Error encrypting file: ' .. input_file)
-  end
+	vim.fn.system(command)
+	if vim.v.shell_error ~= 0 then
+		print("Error encrypting file: " .. input_file)
+	end
 
-   -- reload the buffer to show the encrypted content
-  vim.api.nvim_command('edit')
-
+	-- reload the buffer to show the encrypted content
+	vim.api.nvim_command("edit")
 end
 
 M.file_decrypt = function()
-  local input_file = vim.fn.expand('%:p')
-  debug('decrypting', input_file)
-  local binary = vim.g.nvim_sops_bin_path .. ' '
-  local sops_options = sops.get_sops_general_options()
+	local input_file = vim.fn.expand("%:p")
+	debug("decrypting", input_file)
+	local binary = vim.g.nvim_sops_bin_path .. " "
+	local sops_options = sops.get_sops_general_options()
 
-  local envs = ""
-  for key, value in pairs(sops_options.sopsGeneralEnvVars) do
-    envs = envs .. key .. '=' .. value .. ' '
-  end
-  local args = {
-    '--decrypt',
-    '--in-place',
-  }
+	local envs = ""
+	for key, value in pairs(sops_options.sopsGeneralEnvVars) do
+		envs = envs .. key .. "=" .. value .. " "
+	end
+	local args = {
+		"--decrypt",
+		"--in-place",
+	}
 
-  local command = envs .. binary .. table.concat(args, ' ') .. ' ' .. input_file
-  debug(command)
+	local command = envs .. binary .. table.concat(args, " ") .. " " .. input_file
+	debug("Running command:", command)
 
-  vim.fn.system(command)
-  if vim.v.shell_error ~= 0 then
-    print('Error decrypting file: ' .. input_file)
-  end
+	-- Capture both stdout and stderr
+	local output = vim.fn.system(command)
+	local error_code = vim.v.shell_error
 
-   -- reload the buffer to show the decrypted content
-  vim.api.nvim_command('edit')
+	if error_code ~= 0 then
+		-- Print both the error code and the command output
+		print("Error decrypting file: " .. input_file)
+		print("Error code:", error_code)
+		print("Command output:", output)
+
+		-- Additional debug information
+		debug("Environment variables:")
+		for key, value in pairs(sops_options.sopsGeneralEnvVars) do
+			debug(key .. "=" .. value)
+		end
+		debug("File path:", input_file)
+		return
+	end
+
+	-- Only reload the buffer if decryption was successful
+	vim.api.nvim_command("edit")
 end
 
 return M
